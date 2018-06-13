54e9d28c426f6226f69bc2adcfd29e786fcd76e4
mathiashorn <mathias.horn@gmail.com>
2018-06-13 19:28:30 -0300

    Mensagem 1

--- DotNet.WebApi.AppVeyor.Demo/Controllers/HomeController.cs
+++ DotNet.WebApi.AppVeyor.Demo/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace DotNet.WebApi.AppVeyor.Demo.Controllers
             Message message = new Message();
 
             message.Title = "Olá mundo";
-            message.Content = "Mensagem";
+            message.Content = "Mensagem 1";
 
             return Request.CreateResponse(HttpStatusCode.OK, message);
         }
